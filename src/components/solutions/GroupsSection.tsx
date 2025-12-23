da28d48c4a19f6d66e3e2b444e8c091c0e108fd0
Ganesh <ganeshpsuiux@gmail.com>
2025-12-23 10:16:09 +0530

    button fixed in job categorires

--- src/components/solutions/GroupsSection.tsx
+++ src/components/solutions/GroupsSection.tsx
@@ -163,7 +163,7 @@ const GroupsSection: React.FC = () => {
                 <button className={styles.entryPageButton}>
                     Entry Page
                     <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M5 12H19M19 12L12 5M19 12L12 19" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                        <path d="M5 12H19M19 12L15 8M19 12L15 16" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                     </svg>
                 </button>
             </div>
